9ce5511063dd35f3fd9dad2480fc9dc4bd69dc49
julieta <j.gazzin@gmail.com>
2024-08-22 00:40:50 -0300

    enviroment

--- db/data_base.js
+++ db/data_base.js
@@ -1,8 +1,13 @@
+require('dotenv').config()
+
 const mysql = require('mysql2')
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root'
+    // host: 'localhost',
+    // user: 'root',
+    // password: 'root'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
 })
 
 connection.connect( (err) => {
